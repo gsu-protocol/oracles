0276ea7849b77c74c8d5620913f3c8fb7d560d9c
abbasidd <45910850+abbasidd@users.noreply.github.com>
2023-02-10 17:22:49 +0500

    Add setup scripts. (#4)
    * change nix/sources.json and add setup

---
+++ setup/run_it_relay.sh
@@ -0,0 +1,33 @@
+# the result folder created with nix-build
+while [ -n "$1" ]
+do
+case "$1" in
+--gofer)
+cp -v $2 $(pwd)/result/share/gofer.conf
+export GOFER_CONFIG=$2
+export GOFER_PATH=$(pwd)/result-3/bin/gofer
+shift
+  ;;
+--omnia) 
+cp -v $2 $(pwd)/result/share/relay.conf
+export OMNIA_CONFIG=$2
+export OMNIA_VERBOSE=true
+export OMNIA_PATH=$(pwd)/result-2/bin/omnia
+shift
+;;
+--spire) 
+cp -v $2 $(pwd)/result/share/spire.conf
+export SPIRE_CONFIG=$2
+export SPIRE_PATH=$(pwd)/result-3/bin/spire
+export SPIRE_LOG_VERBOSITY=debug
+shift
+ ;;
+--) shift
+break ;;
+*) echo "$1 is not an option";;
+esac
+shift
+done
+
+export SETZER_MIN_MEDIAN=1
+$(pwd)/result/bin/install-omnia relay
